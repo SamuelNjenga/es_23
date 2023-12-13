dc2f6dcd340bc991544a8ce72bd4e61df9d54d9d
Samuel Njenga <snjenga704@gmail.com>
2023-12-13 13:59:59 +0300

    Feat: add index_three.js

---
+++ index_three.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// Hashbang Grammar -> Hashbangs #! are a sequence of characters at the beginning of a unix-based script which determine what interpreter the script will be executed with
+
+
+console.log('Hello, JS!'); // This JavaScript file can now be executed directly from a Unix-like command line, outputting “Hello, JS!” to the console.
+
